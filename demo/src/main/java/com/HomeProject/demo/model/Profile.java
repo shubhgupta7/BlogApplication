a6d213caaa71c1f595dc539244135a2019073bcf
Shubh <gupta11shubh@gmail.com>
2024-07-11 15:44:52 +0530

    Initial commit

---
+++ demo/src/main/java/com/HomeProject/demo/model/Profile.java
@@ -0,0 +1,15 @@
+package com.HomeProject.demo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Profile {
+    String name;
+    String mail;
+    int age;
+
+    public Profile() {
+    }
+}
